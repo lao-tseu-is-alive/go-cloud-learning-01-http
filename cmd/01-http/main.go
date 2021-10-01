044e96a0c1f71f23946bb4ea906c3b5ee59dab4f
cgil <lao.tseu.is.alive@gmail.com>
2021-10-01 18:42:12 +0200

    added table-driven approach to testing the handler and template with a variable

--- cmd/01-http/main.go
+++ cmd/01-http/main.go
@@ -1,33 +1,78 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
-	"io"
+	"html/template"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"strconv"
+	"strings"
 )
 
 const defaultPort = 8080
+const defaultUserName = "World"
 
-const helloMsg = `
+func getHelloMsg(name string) (string, error) {
+	const helloMsg = `
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css" integrity="sha512-EZLkOqwILORob+p0BXZc+Vm3RgJBOe1Iq/0fiI7r/wJgzOFZMlsqTa29UEl6v6U6gsV4uIpsNZoV32YZqrCRCQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <title>GOLANG Hello World</title>
+    <title>GOLANG Hello {{.UserName}}</title>
   </head>
   <body>
-	<h3>Hello, world!</h3>
+	<h3>Hello, {{.UserName}}!</h3>
   </body>
 </html>
 `
 
+	data := struct {
+		UserName string
+	}{UserName: name}
+	var tpl bytes.Buffer
+	t, err := template.New("hello-page").Parse(helloMsg)
+	if err != nil {
+		return "", err
+	}
+	if err := t.Execute(&tpl, data); err != nil {
+		return "", err
+	}
+
+	return tpl.String(), nil
+}
+
 // helloWorldHandler is a simple http handler to give some greetings with a valid html
 func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, helloMsg)
+	switch r.Method {
+	case http.MethodGet:
+		// retrieve a parameter from query
+		query, err := url.ParseQuery(r.URL.RawQuery)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Bad request: Error in ParseQuery: %q", err), http.StatusBadRequest)
+			return
+		}
+		username := defaultUserName
+		if query.Has("username") {
+			username = query.Get("username")
+		}
+		username = strings.TrimSpace(username)
+		if len(username) == 0 {
+			http.Error(w, fmt.Sprintf("Bad request: Error in query.Get('username'): username cannot be empty or spaces only"), http.StatusBadRequest)
+			return
+		}
+		res, err := getHelloMsg(username)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Internal server error:%q", err), http.StatusInternalServerError)
+			return
+		}
+		fmt.Fprintf(w, res)
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
 }
 
 // let's improve our http server : we will allow it to read
@@ -47,6 +92,14 @@ func main() {
 		}
 		listenAddr = fmt.Sprintf(":%v", port)
 	}
+
+	/* section above is to illustrate and test the template
+	msg, err := getHelloMsg("toto")
+	if err != nil {
+		fmt.Errorf("ERROR doing getHelloMsg : %q", err)
+	}
+	fmt.Println(msg)
+	*/
 
 	http.HandleFunc("/hello", helloWorldHandler)
 	log.Printf("Starting server... try navigating to http://localhost%v/hello to be greeted", listenAddr)
--- cmd/01-http/main_test.go
+++ cmd/01-http/main_test.go
@@ -9,17 +9,77 @@ import (
 // TestHelloWorldHandler allows to check that the HelloWorldHandler works as expected
 // just run : go test
 func TestHelloWorldHandler(t *testing.T) {
-	request, _ := http.NewRequest(http.MethodGet, "/hello", nil)
-	response := httptest.NewRecorder()
 
-	helloWorldHandler(response, request)
-	// what about status code ??
-	t.Run("returns the hello message", func(t *testing.T) {
-		got := response.Body.String()
-		want := helloMsg
+	defaultMsg, _ := getHelloMsg(defaultUserName)
 
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
-	})
+	tt := []struct {
+		name           string
+		method         string
+		paramKeyValues map[string]string
+		want           string
+		statusCode     int
+	}{
+		{
+			name:           "without any username parameter",
+			method:         http.MethodGet,
+			paramKeyValues: make(map[string]string, 0),
+			want:           defaultMsg,
+			statusCode:     http.StatusOK,
+		},
+		{
+			name:           "with username having a valid value",
+			method:         http.MethodGet,
+			paramKeyValues: map[string]string{"username": "Carlos"},
+			want:           "", // let's calculate the result later based on given userName
+			statusCode:     http.StatusOK,
+		},
+		{
+			name:           "with username having an empty value",
+			method:         http.MethodGet,
+			paramKeyValues: map[string]string{"username": ""},
+			want:           "Bad request: Error in query.Get('username'): username cannot be empty or spaces only\n",
+			statusCode:     http.StatusBadRequest,
+		},
+		{
+			name:           "with bad method",
+			method:         http.MethodPost,
+			paramKeyValues: map[string]string{"username": "WhatEverYouWant", "param2": "nobody is here"},
+			want:           "Method not allowed\n",
+			statusCode:     http.StatusMethodNotAllowed,
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			request, _ := http.NewRequest(tc.method, "/hello", nil)
+			username := ""
+			if len(tc.paramKeyValues) > 0 {
+				parameters := request.URL.Query()
+				for paramName, paramValue := range tc.paramKeyValues {
+					parameters.Add(paramName, paramValue)
+					if paramName == "username" {
+						username = paramValue
+					}
+				}
+				request.URL.RawQuery = parameters.Encode()
+			}
+			response := httptest.NewRecorder()
+			helloWorldHandler(response, request)
+			got := response.Body.String()
+			want := tc.want
+			if len(want) == 0 {
+				// let's calculate the result later based on current username in test array
+				want, _ = getHelloMsg(username)
+			}
+			// testing the response body
+			if got != want {
+				t.Errorf("in response.Body got %q, want %q", got, want)
+			}
+			// testing the server status code
+			if response.Code != tc.statusCode {
+				t.Errorf("in response.Code got %d, want status %d", response.Code, tc.statusCode)
+			}
+
+		})
+	}
 }
